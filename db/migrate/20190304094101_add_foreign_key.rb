cdf6ab0ed19cd7bcf26f0e22b155c9f9240aa86f
yuneszadi <yuneszadi@gmail.com>
2019-03-04 11:43:00 +0200

    Rails Model

---
+++ db/migrate/20190304094101_add_foreign_key.rb
@@ -0,0 +1,7 @@
+class AddForeignKey < ActiveRecord::Migration[5.2]
+  def change
+    add_foreign_key :tests, :categories
+    add_foreign_key :questions, :tests
+    add_foreign_key :answer, :questions
+  end
+end
