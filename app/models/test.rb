ad07bd18478af12d9da7bbf339d827f694ebda3c
yuneszadi <yuneszadi@gmail.com>
2019-03-04 11:46:16 +0200

    Active Record

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.find_by_category(category)
+    joins('JOIN categories ON categories.id = tests.category_id')
+      .where(categories: { title: category })
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  def find_by_level(level)
+    Test.joins('JOIN user_tests ON user_tests.test_id = tests.id').where(
+      level: level,
+      user_tests: { user_id: id }
+    )
+  end
 end
